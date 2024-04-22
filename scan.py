c91120b16b9d2427743099335f48fa1000e814c8
Damian Duda <damian.duda.89@gmail.com>
2024-04-22 11:01:39 +0200

    Get rid of MessageGroupId

--- scan.py
+++ scan.py
@@ -165,7 +165,6 @@ def sns_start_scan(sns_client, s3_object, scan_start_sns_arn, timestamp):
     }
     sns_client.publish(
         TargetArn=scan_start_sns_arn,
-        MessageGroupId=s3_object.key,
         Message=json.dumps({"default": json.dumps(message)}),
         MessageStructure="json",
     )
@@ -192,7 +191,6 @@ def sns_scan_results(
     }
     sns_client.publish(
         TargetArn=sns_arn,
-        MessageGroupId=s3_object.key,
         Message=json.dumps({"default": json.dumps(message)}),
         MessageStructure="json",
         MessageAttributes={
